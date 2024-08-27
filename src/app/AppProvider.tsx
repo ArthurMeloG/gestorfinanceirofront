1ef4bc07762b9a5cface042a7f2014b7c8cd7f52
ArthurMeloG <arthur.garcia@opensingular.com>
2024-08-27 09:35:39 -0300

    App provider

---
+++ src/app/AppProvider.tsx
@@ -0,0 +1,13 @@
+import {createContext, useContext, useState, useState} from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+    return (
+        <AppContext.Provider value={}>
+            {children}
+        </AppContext.Provider>
+    )
+};
+
+export const useAppContext = () => useContext(AppContext);
